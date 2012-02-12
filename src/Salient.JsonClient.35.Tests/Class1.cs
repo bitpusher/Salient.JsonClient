ab220b0a54989abf4f0b4efa46fbcf73530eb0fe
bitpusher <sky.sanders@gmail.com>
2012-02-12 16:30:46 -0700

    first commit

---
+++ src/Salient.JsonClient.35.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Salient.JsonClient._35.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Salient.JsonClient.35/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Salient.JsonClient._35
+{
+    public class Class1
+    {
+    }
+}
